415c0bd8d206c1094e9c19b03902aaf458dfec3c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-23 21:57:12 +0300

    Async. functions
    first Demo

---
+++ Lecture5-AsynchPrograming/firstDemo.js
@@ -0,0 +1,11 @@
+console.log('Hello');
+
+setTimeout(()=>{
+    console.log('What are you gonna do aboyt it?');
+}, 6000);
+setTimeout(function() {
+    console.log('Middle Eart is gonna die !!!');
+}, 4000);
+
+
+console.log('End of the world is here');
