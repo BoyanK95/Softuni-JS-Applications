81a8719ff85bd54d6132f072654527f3d24ff8f8
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-01 02:15:42 +0200

    Created today div

--- AsynchProgramingExercise_6/01.Bus-Stop/app.js
+++ AsynchProgramingExercise_6/01.Bus-Stop/app.js
@@ -1,3 +1,27 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopIDInput = document.getElementById('stopId');
+    const stopId = stopIDInput.value
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+    const stopNameEl = document.getElementById('stopName')
+    const busList = document.getElementById('buses')
+    
+    busList.innerHTML = ''
+    stopId.value = ''
+    try{
+        const responce = await fetch(url)
+        const data = await responce.json()
+    
+    
+        stopNameEl.textContent = data.name
+    
+        Object.entries(data.buses).forEach(([busNumber, timeArrive]) =>{
+            const li = document.createElement('li')
+            li.textContent = `Bus ${busNumber} arrives in ${timeArrive} minutes`
+            busList.appendChild(li)
+        })
+    } catch (e) {
+        stopNameEl.textContent = 'Error'
+    }
+
+    
 }
--- AsynchProgramingExercise_6/03.Forecaster/app.js
+++ AsynchProgramingExercise_6/03.Forecaster/app.js
@@ -1,5 +1,78 @@
+const enumIcon = {
+  "Sunny": "&#x2600", // ☀
+  "Partly sunny": "&#x26C5", // ⛅
+  "Overcast": "&#x2601", // ☁
+  "Rain": "&#x2614", // ☂
+  "Degrees": "&#176", // °
+};
+
 function attachEvents() {
-    console.log("TODO...");
+  document.getElementById("submit").addEventListener("click", getWeather);
+}
+async function getWeather() {
+  const url = "http://localhost:3030/jsonstore/forecaster/locations";
+  const cityName = document.getElementById("location").value;
+
+  const responce = await fetch(url);
+  const data = await responce.json();
+
+  const info = data.find((x) => x.name === cityName);
+
+  createForecaster(info.code);
+}
+
+async function createForecaster(code) {
+  const currentSection = document.getElementById("current");
+  const forecastContainer = document.getElementById('forecast')
+  forecastContainer.style.display = 'block';
+
+  const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
+  const urlUpcoming = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
+
+  //TODO use Promise.all
+
+  const resToday = await fetch(urlToday);
+  const dataToday = await resToday.json();
+
+  const resUpcoming = await fetch(urlUpcoming);
+  const dataUpcoming = await resUpcoming.json();
+
+  const todayHtmlTemp = createToday(dataToday);
+  currentSection.appendChild(todayHtmlTemp)
+}
+
+function createToday(data) {
+  const { condition, high, low } = data.forecast;
+  debugger
+  const conditionContainer = document.createElement("div");
+  conditionContainer.classList.add("forecast");
+
+  const conditionIconSpan = document.createElement("span");
+  conditionIconSpan.classList.add("condition", 'symbol');
+  conditionIconSpan.innerHTML = enumIcon[condition];
+
+  const conditionSpan = document.createElement("span");
+  conditionSpan.classList.add("condition");
+
+  const nameSpan = document.createElement("span");
+  nameSpan.classList.add("forecast-data");
+  nameSpan.textContent = data.name;
+
+  const tempSpan = document.createElement("span");
+  tempSpan.classList.add("forecast-data");
+  tempSpan.innerHTML = `${low}${enumIcon["Degrees"]}/${high}${enumIcon["Degrees"]}`;
+
+  const conditionTxtSpan = document.createElement("span");
+  conditionTxtSpan.classList.add("forecast-data");
+  conditionTxtSpan.textContent = condition;
+
+  conditionSpan.appendChild(nameSpan);
+  conditionSpan.appendChild(tempSpan);
+  conditionSpan.appendChild(conditionTxtSpan);
+
+  conditionContainer.appendChild(conditionIconSpan);
+  conditionContainer.appendChild(conditionSpan);
+  return conditionContainer;
 }
 
 attachEvents();
