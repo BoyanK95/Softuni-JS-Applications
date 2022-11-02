d221d376409e9195350928c308438e3615199d0b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-02 03:30:02 +0200

    Made a simple post and get comments page

--- Lecture7RemoteData/app.js
+++ Lecture7RemoteData/app.js
@@ -1,12 +1,45 @@
+init();
+
 function init() {
-    
+    document.getElementById('load').addEventListener('click', async ()=>{
+        const comments = await getComments();
+        displayComments(comments);
+    });
 }
 
-function getComments() {
-    
+function displayComments(comments) {
+    const list = document.getElementById('comments')
+    list.replaceChildren(...comments.map(createCommentCard))
+}
+
+function createCommentCard(comment) {
+    const element = document.createElement('article')
+    element.innerHTML = `<header><h3>${comment.name}</h3></header>
+    <main><p>${comment.content}</p></main>`;
+
+    return element
+}
+
+async function getComments() {
+    //http://localhost:3030/jsonstore/comments
+
+    const responce = await fetch('http://localhost:3030/jsonstore/comments');
+    const data = await responce.json();
+
+    return Object.values(data)
 }
 
 
-function postComment() {
-    
+async function postComment(comment) {
+    const responce = await fetch('http://localhost:3030/jsonstore/comments',{
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    });
+
+    const data = await responce.json()
+
+    return data
 }
