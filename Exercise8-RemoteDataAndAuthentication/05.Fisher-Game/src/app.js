908bf378f24768aaebfce7f591da95b757030a03
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-04 01:27:59 +0200

    FisherGame App

--- Exercise8-RemoteDataAndAuthentication/05.Fisher-Game/src/app.js
+++ Exercise8-RemoteDataAndAuthentication/05.Fisher-Game/src/app.js
@@ -1,1 +1,11 @@
-console.log('TODO:// Implement Home functionality');
+window.addEventListener('DOMContentLoaded', onLoadHtml)
+
+function onLoadHtml() {
+    const token = sessionStorage.getItem('accessToken')
+    if (token) {
+        document.getElementById('guest').style.display = 'none'
+    }else{
+        document.getElementById('guest').style.display = 'incline-block'
+        
+    }
+}
--- Exercise8-RemoteDataAndAuthentication/05.Fisher-Game/src/register.js
+++ Exercise8-RemoteDataAndAuthentication/05.Fisher-Game/src/register.js
@@ -1,1 +1,57 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById('register-form').addEventListener('submit', registerHandler)
+document.querySelectorAll('a').forEach(x=> x.classList.remove('active'))
+document.getElementById('register').classList.add('active')
+const errorP = document.querySelector('p.notification')
+
+
+function registerHandler(e) {
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const {email, password, rePasswod} = Object.fromEntries(formData)
+    if (password !== rePasswod) {
+        errorP.textContent = 'Error!'
+        setTimeout(()=>{
+            errorP.textContent = ''
+        }, 1000)
+    }else{
+        onRegister(email, password)
+    }
+}
+
+async function onRegister(email, password) {
+    //TODO Error handling
+    const url = 'http://localhost:3030/users/register'
+
+    const body = {
+        email,
+        password
+    }
+    const header = getHeader('POST', body)
+    try {
+        const res = await fetch(url, header)
+        const data = await res.json()
+        if (data.code &&data.code !== 200) {
+            throw new Error(data.message)
+        }
+        sessionStorage.setItem('email', data.email)
+        sessionStorage.setItem('accessToken', data.accessToken)
+        return data
+    }
+    catch(e){
+        errorP.textContent = e
+        setTimeout(()=>{
+            errorP.textContent = ''
+        },1000)
+    }
+}
+
+function getHeader(method, body) {
+    return {
+        method: `${method}`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+}
