177027dc84e16176b1be7e138442052cfbf506ec
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-06 21:14:00 +0200

    Some fixes to SWAPI

--- Lecture5-AsynchPrograming/swapi/appSwapi.js
+++ Lecture5-AsynchPrograming/swapi/appSwapi.js
@@ -1,6 +1,18 @@
 document
   .getElementById("load-charachter")
-  .addEventListener("click", loadCharachter);
+  .addEventListener("click", asyncLoadCharachter);
+
+  const displayArea = document.getElementById('charachters-area')
+
+  async function asyncLoadCharachter() {
+    const res = await fetch(`https://swapi.dev/api/people/` + document.getElementById('idNum').value)
+    const data = await res.json()
+    console.log(data);
+    let personName = JSON.stringify(data.name);
+    let personHeight = JSON.stringify(data.height);
+
+    displayArea.value = `This ${personName} is ${personHeight}m tall and has ${data.skin_color} skin and is of ${data.gender} gender!`
+  }
 
 function loadCharachter() {
   let baseUrl = "https://swapi.dev/api";
