6bd89f7ff71c533089b2e25f61af9706c5ced034
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-07 17:55:21 +0200

    Bus-Schedule

--- Exercise6-AsynchPrograming/02.Bus-Schedule/app.js
+++ Exercise6-AsynchPrograming/02.Bus-Schedule/app.js
@@ -1,11 +1,28 @@
 function solve() {
+    const url = 'http://localhost:3030/jsonstore/bus/schedule/'
+    const infoDiv = document.querySelector('.info')
+    const departBtn = document.getElementById('depart')
+    const arriveBtn = document.getElementById('arrive')
 
-    function depart() {
-        console.log('Depart TODO...');
+    let Id = 'depot'
+    let stopName = ''
+
+    async function depart() {
+        const responce = await fetch(`${url}${Id}`)
+        const res = await responce.json()
+        stopName = res.name
+        
+        infoDiv.textContent = `Next stop ${stopName}`
+        departBtn.disabled = true
+        arriveBtn.disabled = false
+        
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoDiv.textContent = `Arriving at ${stopName}`;
+        
+        departBtn.disabled = false
+        arriveBtn.disabled = true
     }
 
     return {
