4ab005fae05373d73dc1fb96e77a5f32b6cc3271
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-08 22:31:09 +0200

    SPA Forum finished

--- Exercise10-SPA/01.Forum/src/details.js
+++ Exercise10-SPA/01.Forum/src/details.js
@@ -1,35 +1,140 @@
-const section = document.getElementById('detailsView')
-const main = document.getElementsByTagName('main')[0]
+const section = document.getElementById("detailsView");
+const main = document.getElementsByTagName("main")[0];
+const form = section.querySelector("form");
+form.addEventListener("submit", onSubmit);
+const themeContentWrapper = document.getElementById("theme-container-wrapper");
 
-section.remove()
+section.remove();
+let id;
 
 export async function showDetails(e) {
-    if (!e) {
-        return
-    }
-    let id;
-    if (e.target.tagName == 'H2') {
-        id = e.target.parentElement.id
-    } else if (e.target.tagName == 'A'){
-        id = e.target.id
-    }
-    const topic = loadTopic(id)
-    //const comments = loadComment(id)
-    //const res = topicTemplate(topic, comments) //tbd
-    //section.replaceChildren(res)
+  if (!e) {
+    return;
+  }
+  if (e.target.tagName == "H2") {
+    id = e.target.parentElement.id;
+  } else if (e.target.tagName == "A") {
+    id = e.target.id;
+  }
 
-    main.replaceChildren(section)
+  appendElement();
 }
 
-function topicTemplate(data) {
-    
+async function appendElement() {
+  const topic = await loadTopic(id);
+  const comments = await loadComment(id);
+
+  const res = topicTemplate(topic, comments); //tbd
+  themeContentWrapper.replaceChildren(res);
+  main.replaceChildren(section);
+}
+
+function topicTemplate(topic, comments) {
+  const topicContainer = document.createElement("div");
+  topicContainer.classList.add("theme-title");
+  topicContainer.innerHTML = `
+                <div class="theme-name-wrapper">
+                    <div class="theme-name">
+                        <h2>${topic.topicName}</h2>
+                    </div>
+                </div>
+   `;
+
+  const commentContainer = document.createElement("div");
+  commentContainer.classList.add("comment");
+
+  commentContainer.innerHTML = `
+   <div class="header">
+        <img src="./static/profile.png" alt="avatar">
+        <p><span>${topic.username}</span> posted on <time>${topic.date}</time></p>
+
+   <p class="post-content">${topic.postText}</p>
+    </div>
+   `;
+
+  comments.forEach((x) => {
+    const comment = createComment(x);
+    commentContainer.appendChild(comment);
+  });
+  return commentContainer;
+}
+
+function createComment(data) {
+  const container = document.createElement("div");
+  container.classList.add("user-comment");
+  container.innerHTML = `
+    <div class="topic-name-wrapper">
+            <div class="topic-name">
+                <p><strong>${data.username}</strong> commented on <time>${data.date}</time></p>
+            <div class="post-content">
+                    <p>${data.postText}</p>
+            </div>
+        </div>
+    </div>`;
+
+  return container;
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const { postText, username } = Object.fromEntries(formData);
+  createPost({ postText, username, id, date: new Date() });
+}
+
+async function createPost(body) {
+  const url = "http://localhost:3030/jsonstore/collections/myboard/comments";
+
+  try {
+    const responce = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    if (!responce.ok) {
+      const err = await responce.json();
+      throw new Error(err.message);
+    }
+    appendElement();
+    clearForm();
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function clearForm() {
+  form.reset();
+}
+
+async function loadComment(id) {
+  const url = "http://localhost:3030/jsonstore/collections/myboard/comments"; //TODO id?
+  try {
+    const responce = await fetch(url);
+    const data = await responce.json();
+    if (!responce.ok) {
+      const error = data;
+      throw new Error(error.message);
+    }
+
+    const filterData = Object.values(data).filter((x) => x.id === id);
+    return filterData;
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 async function loadTopic(id) {
-    const url = `http://localhost:3030/jsonstore/collections/myboard/posts/${id}`
+  const url = `http://localhost:3030/jsonstore/collections/myboard/posts/${id}`;
 
-    const responce = await fetch(url)
-    const data = await responce.json()
-
-    return data
+  try {
+    const responce = await fetch(url);
+    const data = await responce.json();
+    if (!responce.ok) {
+      const error = data;
+      throw new Error(error.message);
+    }
+    return data;
+  } catch (error) {
+    alert.message;
+  }
 }
--- Exercise10-SPA/01.Forum/src/home.js
+++ Exercise10-SPA/01.Forum/src/home.js
@@ -1,28 +1,27 @@
-import { showDetails } from './details.js'
+import { showDetails } from "./details.js";
 
-const section = document.getElementById('homeView')
-// 
-const main = document.getElementsByTagName('main')[0]
-const form = main.querySelector('form')
-form.addEventListener('submit', onSubmit)
-const url = 'http://localhost:3030/jsonstore/collections/myboard/posts'
+const section = document.getElementById("homeView");
+//
+const main = document.getElementsByTagName("main")[0];
+const form = document.querySelector("#homeView form");
+form.addEventListener("submit", onSubmit);
+const url = "http://localhost:3030/jsonstore/collections/myboard/posts";
 
-
+section.remove();
 
 export async function showHome() {
-    const topicContainer = section.querySelector('.topic-title')
+  const topicContainer = section.querySelector(".topic-title");
 
-    const posts = await loadPost()
-    const content = Object.values(posts).map(x => topicTamplete(x))
-    topicContainer.replaceChildren(...content)
-    main.replaceChildren(section)
+  const posts = await loadPost();
+  const content = Object.values(posts).map((x) => topicTamplete(x));
+  topicContainer.replaceChildren(...content);
+  main.replaceChildren(section);
 }
 
-
 function topicTamplete(data) {
-    const container = document.createElement('div')
-    container.classList.add('topic-container')
-    container.innerHTML = `
+  const container = document.createElement("div");
+  container.classList.add("topic-container");
+  container.innerHTML = `
     <div class="topic-name-wrapper">
         <div class="topic-name">
             <a href="#" class="normal" id="${data._id}">
@@ -37,46 +36,61 @@ function topicTamplete(data) {
                 </div>
             </div>
             </div>
-        </div>`
+        </div>`;
 
-        container.querySelector('a').addEventListener('click', showDetails)
-    return container
+  container.querySelector("a").addEventListener("click", showDetails);
+  return container;
 }
 
 function onSubmit(e) {
-    e.preventDefault()
-    if (e.submitter.innerHTML === 'Cancel') {
-      return clearForm()
-    }
-    const formData = new FormData(form)
-    const { topicName, username, postText } = Object.fromEntries(formData)
-    
-    createPost({ topicName, username, postText, date: new Date() })
-    clearForm()
+  e.preventDefault();
+  if (e.submitter.innerHTML === "Cancel") {
+    return clearForm();
+  }
+  const formData = new FormData(form);
+  const { topicName, username, postText } = Object.fromEntries(formData);
+
+  createPost({ topicName, username, postText, date: new Date() });
+  clearForm();
 }
 
 function clearForm() {
-    form.reset()
+  form.reset();
 }
 
 async function createPost(body) {
+  try {
     const responce = await fetch(url, {
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify(body)
-    })
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(body),
+    });
 
-    const data = await responce.json()
-    return data
+    const data = await responce.json();
+    if (!responce.ok) {
+      const error = data.message;
+      throw new Error(error);
+    }
+
+    showHome();
+  } catch (error) {
+    alert(error.message)
+  }
 }
 
 async function loadPost() {
-    const responce = await fetch(url)
-    const data = await responce.json()
-
-    return data
+    try{
+        const responce = await fetch(url);
+        const data = await responce.json();
+        if (!responce.ok) {
+            const err = responce.message
+            throw new Error(err)
+        }
+        return data;
+    }catch(err){
+        alert(err.message)
+    }
 }
 
-window.loadPost = loadPost
 
-showDetails()
+showDetails();
