d1f8fee54dfb4bbcf070d061ba0137654df20c76
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-11 06:14:17 +0200

    created the different views

--- Exercise12V-2/03.SoftTerest/app.js
+++ Exercise12V-2/03.SoftTerest/app.js
@@ -1,29 +1,33 @@
-import { showHome } from "./src/home.js"
+import { showCatalog } from "./src/views/catalog.js"
+import { showCreate } from "./src/views/create.js"
+import { showDetails } from "./src/views/details.js"
+import { showHome } from "./src/views/home.js"
+import { showLogin } from "./src/views/login.js"
+import { showRegister } from "./src/views/register.js"
 
 const main = document.getElementById('mainView')
 
-const registerView = document.getElementById('registerView')
-const loginView = document.getElementById('loginView')
-const dashboard = document.getElementById('dashboard-holder')
-const detailsView = document.getElementById('detailsView')
-const createView = document.getElementById('createView')
+// const registerView = document.getElementById('registerView')
+// const loginView = document.getElementById('loginView')
+// const dashboard = document.getElementById('dashboard-holder')
+// const detailsView = document.getElementById('detailsView')
+// const createView = document.getElementById('createView')
 const defSection = document.getElementById('defSection').remove()
 
 const link = {
     '/': showHome,
-    '/catalog': dashboard,
-    '/login': loginView,
-    '/register': registerView,
-    '/details': detailsView,
-    '/create': createView,
+    '/catalog': showCatalog,
+    '/login': showLogin,
+    '/register': showRegister,
+    '/details': showDetails,
+    '/create': showCreate,
 }
 
 const context = {
     showSection
 }
 
-window.showHome = showHome
-window.context = context
+
 
 function showSection(section) {
     main.replaceChildren(section)
