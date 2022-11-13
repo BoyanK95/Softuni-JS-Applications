2bed7325b4cd69cd7e866209e486100b017c5c6f
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-14 00:51:05 +0200

    Created accordionFunctionality

--- Exercise6-AsynchPrograming/05. Accordion/accordion.js
+++ Exercise6-AsynchPrograming/05. Accordion/accordion.js
@@ -1,3 +1,55 @@
 function solution() {
-    //TODO .....
+    const baseUrl = 'http://localhost:3030/jsonstore/advanced/articles/list'
+    const mainRoot = document.getElementById('main')
+    createArticle(getUrl())
+
+    function createArticle(data) {
+        const divAccordion = document.createElement('div')
+        divAccordion.classList.add('accordion')
+        data.forEach(article => {
+            let headDiv = document.createElement('div')
+            headDiv.className = 'head'
+            let spanEl = document.createElement('span')
+            spanEl.textContent = article.title
+            let btn = document.createElement('button')
+            btn.setAttribute('id', article._id)
+            btn.setAttribute('class', 'button')
+            btn.textContent = 'More'
+            headDiv.appendChild(spanEl)
+            headDiv.appendChild(btn)
+            let extraDiv = document.createElement('div')
+            let extraP = document.createElement('p')
+            extraP.textContent = getUrl()
+
+        });
+        
+    //         <span>Scalable Vector Graphics</span>
+    //         <button class="button" id="ee9823ab-c3e8-4a14-b998-8c22ec246bd3">More</button>
+    //     </div>
+    //     <div class="extra">
+    //         <p>Scalable Vector Graphics .....</p>
+
+    }
+
+    async function getUrl(id = null ? null : id) {
+        try{
+            if (!responce.ok) {
+                const err = await responce.json()
+                throw new Error(err.message)
+            }
+            if (id) {
+            const responce = await fetch(baseUrl + '/' +id)
+            const data = responce.json()
+            return data
+            }else {
+                const responce = await fetch(baseUrl)
+                const data = responce.json()
+                return data
+            }
+
+
+        }catch(err){
+            window.alert(err.message)
+        }
+    }
 }
