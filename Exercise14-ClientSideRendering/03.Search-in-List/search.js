6c946dded13c36e8325ef61b6f07a8bfeb8f6973
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-14 20:01:00 +0200

    Finished search-in-list functionality

--- Exercise14-ClientSideRendering/03.Search-in-List/search.js
+++ Exercise14-ClientSideRendering/03.Search-in-List/search.js
@@ -1,3 +1,46 @@
-function search() {
-   // TODO
+import { html, render} from './node_modules/lit-html/lit-html.js'
+import { towns } from './towns.js'
+
+const townsRoot = document.getElementById('towns')
+const resultRoot = document.getElementById('result')
+const btn = document.querySelector('button')
+btn.addEventListener('click', search)
+
+update()
+
+function searchTemplate(towns, match) {
+   const ul = html`
+   <ul>
+      ${towns.map(town => createLiTemplate(town, match))}
+   </ul>
+   `
+   return ul
 }
+
+function createLiTemplate(town, match) {
+   return html`
+         <li class="${match && town.toLowerCase().includes(match) ? 'active' : ''}">${town}</li>
+   `
+}
+
+function update(text) {
+   const ul = searchTemplate(towns, text)
+   render(ul, townsRoot)
+}
+
+function search(e) {
+   const textInput = document.getElementById('searchText')
+   const text = textInput.value.toLowerCase()
+   update(text)
+   updateCount()
+   textInput.value = ''
+}
+
+function updateCount() {
+   const count = document.querySelectorAll('.active').length
+   const countEl = count ? html`<p>${count} matches found</p>` : ''
+
+   render(countEl, resultRoot)
+}
+
+
