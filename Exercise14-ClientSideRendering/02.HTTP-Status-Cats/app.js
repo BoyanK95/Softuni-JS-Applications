b1ad5b2b65e8faca7ce0a2a67da4ced7156b3f39
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-15 02:27:54 +0200

    Created showContent function and finished cats http exercise

--- Exercise14-ClientSideRendering/02.HTTP-Status-Cats/app.js
+++ Exercise14-ClientSideRendering/02.HTTP-Status-Cats/app.js
@@ -16,18 +16,30 @@ function createCatCard(cat) {
   return html`
     <li>
       <img
-        src="./images/cat100.jpg"
+        src="./images/${cat.imageLocation}.jpg"
         width="250"
         height="250"
         alt="Card image cap"
       />
       <div class="info">
-        <button class="showBtn">Show status code</button>
-        <div class="status" style="display: none" id="100">
-          <h4>Status Code: 100</h4>
-          <p>Continue</p>
+        <button @click=${showContent} class="showBtn">Show status code</button>
+        <div class="status" style="display: none" id=${cat.id}>
+          <h4>Status Code: ${cat.statusCode}</h4>
+          <p>${cat.statusMessage}</p>
         </div>
       </div>
     </li>
   `;
+}
+
+function showContent(e) {
+    const btn = e.target
+    const hiddenDiv = e.target.parentElement.querySelector('div')
+    if (hiddenDiv.style.display == 'none') {
+        hiddenDiv.style.display = 'block'
+        btn.textContent = 'Hide status cod'
+    }else{
+        hiddenDiv.style.display = 'none'
+        btn.textContent = 'Show status code'
+    }
 }
