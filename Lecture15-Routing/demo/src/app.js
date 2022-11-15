9b64be77da560cfa8a7419aefa5c61875c437b3a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-16 01:26:22 +0200

    Routing

--- Lecture15-Routing/demo/src/app.js
+++ Lecture15-Routing/demo/src/app.js
@@ -1,2 +1,44 @@
-//detect URL changes notify application
-//change URL on application content swap
+//Detect URL changes notify application
+//Change URL on application content swap
+
+
+const views = {
+    '/': () => '<h2>Home Page</h2>',
+    '/catalog': () => '<h2>Catalog</h2>',
+    '/about': () => '<h2>About Page</h2>',
+}
+
+const main = document.querySelector('main')
+document.querySelector('nav').addEventListener('click',onNavigate)
+window.addEventListener('popstate', ()=> onPopState)
+
+onPopState()
+
+function onNavigate(e) {
+    if (e.target.tagName == 'A') {
+        const url = new URL(e.target.href)
+        if (showView(url.pathname)) {
+            e.preventDefault()
+            history.pushState(null, '', url.pathname)
+            debugger
+        }
+    }
+}
+
+function onPopState(params) {
+    //Start application in home view
+    const startingView = window.location.pathname
+    debugger
+    showView(startingView)
+}
+
+
+function showView(name) {
+    const view = views[name]
+    if (typeof view == 'function') {
+        main.innerHTML = view()
+        return true
+    }else {
+        return false
+    }
+}
