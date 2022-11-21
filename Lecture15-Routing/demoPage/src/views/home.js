3d683e8e249221d725913ac06bbd37e5b1472974
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-21 18:21:14 +0200

    ModularApp Exercise Resources

--- Lecture15-Routing/demoPage/src/views/home.js
+++ Lecture15-Routing/demoPage/src/views/home.js
@@ -1,7 +1,9 @@
 import { html } from "../../node_modules/lit-html/lit-html.js"
 
 
-const homeTemplate = () => html`<h2>Home Page</h2>`
+const homeTemplate = () => html`
+<h2>Home Page</h2>
+<h3>This is my Page Demo cook book SPA</h3>`
 
 
 
