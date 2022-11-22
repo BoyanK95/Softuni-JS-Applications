aeb0b5b00d20c2ed4d337f815a9db3585e52d768
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-23 01:53:26 +0200

    Created loginView template

--- ExamPrep19/src/app.js
+++ ExamPrep19/src/app.js
@@ -1,6 +1,7 @@
 import { render, page } from "./lib.js";
 import { showCatalog } from "./views/catalog.js";
 import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
 
 const main = document.getElementById('content')
 
@@ -11,7 +12,7 @@ page('/catalog', showCatalog)
 page('/catalog/:id', ()=> console.log('details'))
 page('/edit/:id', ()=> console.log('edit'))
 page('/create', ()=> console.log('create'))
-page('/login', ()=> console.log('login'))
+page('/login', showLogin)
 page('/register', ()=> console.log('register'))
 
 page.start()
