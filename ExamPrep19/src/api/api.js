b7910d8af8af4d5a474e0c7d80e22c78d228c9b3
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-22 18:55:16 +0200

    Create src file initial requester and fixed npm librares

---
+++ ExamPrep19/src/api/api.js
@@ -0,0 +1,16 @@
+import { getUserData } from "./util"
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers : {}
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(data)
+    }
+
+    const user = getUserData()
+
+}
