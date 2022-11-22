1182ae11d9e269f4d0da4b14e32ce19b7b58f8d1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-23 00:37:41 +0200

    Created homeView

--- ExamPrep19/src/app.js
+++ ExamPrep19/src/app.js
@@ -1,6 +1,11 @@
 import { render, page } from "./lib.js";
+import { showHome } from "./views/home.js";
 
-page('/', ()=> console.log('home'))
+const main = document.getElementById('content')
+
+
+page(decorateContent)
+page('/', showHome)
 page('/catalog', ()=> console.log('catalog'))
 page('/catalog/:id', ()=> console.log('details'))
 page('/edit/:id', ()=> console.log('edit'))
@@ -9,3 +14,13 @@ page('/login', ()=> console.log('login'))
 page('/register', ()=> console.log('register'))
 
 page.start()
+
+function decorateContent(ctx, next) {
+    ctx.render = renderMain
+
+    next()
+}
+
+function renderMain(content) {
+    render(content, main)
+}
