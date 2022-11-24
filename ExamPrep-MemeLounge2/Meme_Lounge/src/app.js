5d96a88f9fe113ead0b9b092579eedb0004488d1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-24 17:10:03 +0200

    Created homeView

--- ExamPrep-MemeLounge2/Meme_Lounge/src/app.js
+++ ExamPrep-MemeLounge2/Meme_Lounge/src/app.js
@@ -1,8 +1,29 @@
 import {page, render} from './lib.js'
+import { renderHomeView } from './views/home.js'
 
-page('/', () => console.log('home'))
+const main = document.querySelector('main')
+
+page(decorateContect)
+page('/', renderHomeView)
+page('/memes', () => console.log('catalog'))
+page('/memes/:id', () => console.log('details'))
+page('/edit/:id', () => console.log('edit'))
+page('/login', () => console.log('login'))
+page('/register', () => console.log('register'))
+page('/create', () => console.log('create'))
+page('/profile', () => console.log('profile'))
 
 
 //Start application
 page.start()
+
+function decorateContect(ctx, next) {
+    ctx.render = renderMain
+
+    next()
+}
+
+function renderMain(templateResult) {
+    render(templateResult, main)
+}
 
