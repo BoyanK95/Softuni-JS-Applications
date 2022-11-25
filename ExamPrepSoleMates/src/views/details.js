1d26a67777a021e254e9fa5c56f1a8783a14289a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-26 01:23:25 +0200

    Finished detailsView

--- ExamPrepSoleMates/src/views/details.js
+++ ExamPrepSoleMates/src/views/details.js
@@ -2,37 +2,31 @@ import { deleteById, getById } from '../api/data.js'
 import { html, nothing } from '../lib.js'
 
 const detailsTemplate = (item, isOwner, onDelete) => html`
-        <section id="detailsPage">
-            <div class="wrapper">
-                <div class="albumCover">
-                    <img src=${item.imgUrl}>
-                </div>
-                <div class="albumInfo">
-                    <div class="albumText">
-
-                        <h1>Name: ${item.name}</h1>
-                        <h3>Artist: ${item.artist}</h3>
-                        <h4>Genre: ${item.genre}</h4>
-                        <h4>Price: ${item.price}</h4>
-                        <h4>Date: ${item.releaseDate}</h4>
-                        <p>Description: ${item.description}</p>
-                    </div>
-
-                          <!-- Only for registered user and creator of the album-->
-                    ${isOwner ?
-                    html`
-                    <div class="actionBtn">
-                        <a href="/edit/${item._id}" class="edit">Edit</a>
-                        <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>
-                    </div>`:
-                    nothing}
-                  
-                    
-                </div>
+        <section id="details">
+          <div id="details-wrapper">
+            <p id="details-title">Shoe Details</p>
+            <div id="img-wrapper">
+              <img src=${item.imageUrl} alt="example1" />
             </div>
+            <div id="info-wrapper">
+              <p>Brand: <span id="details-brand">${item.brand}</span></p>
+              <p>
+                Model: <span id="details-model">${item.model}</span>
+              </p>
+              <p>Release date: <span id="details-release">${item.release}</span></p>
+              <p>Designer: <span id="details-designer">${item.designer}</span></p>
+              <p>Value: <span id="details-value">${item.value}</span></p>
+            </div>
+             <!--Edit and Delete are only for creator-->
+                ${isOwner ? html`
+                <div id="action-buttons">
+              <a href="/edit/${item._id}" id="edit-btn">Edit</a>
+              <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>
+            </div>` : nothing}           
+          </div>
         </section>`
 
-function itemControls(item, hasUser, onDelete) {
+function itemControls(item, isOwner, onDelete) {
     if (hasUser == false) {
         return nothing
     }
