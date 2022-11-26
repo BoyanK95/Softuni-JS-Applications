739f49eadc62ea93cf53a27a8947268506eaad6b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-26 02:10:33 +0200

    Finished searchView

--- ExamPrepSoleMates/src/app.js
+++ ExamPrepSoleMates/src/app.js
@@ -24,7 +24,7 @@ page('/create', showCreate)
 page('/details/:id', showDetails)
 page('/register', showRegister)
 page('/edit/:id', showEdit)
-page('/search', () => console.log('search'))
+page('/search', showSearch)
 
 updateNav()
 page.start()
--- ExamPrepSoleMates/src/views/search.js
+++ ExamPrepSoleMates/src/views/search.js
@@ -1,64 +1,65 @@
 import { html, nothing } from '../lib.js'
 import { search } from '../api/data.js'
+import { getUserData } from '../util.js'
 
 
-const searchTemplate = (isClicked, showSearch, result, hasUser) => html`
-<section id="searchPage">
-            <h1>Search by Name</h1>
+const searchTemplate = (isClicked, onSearch, result, user) => html`
+<section id="search">
+          <h2>Search by Brand</h2>
 
-            <div class="search">
-                <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
-                <button @click=${showSearch} class="button-list">Search</button>
-            </div>
+          <form class="search-wrapper cf">
+            <input
+              id="#search-input"
+              type="text"
+              name="search"
+              placeholder="Search here..."
+              required
+            />
+            <button @click=${onSearch} type="submit">Search</button>
+          </form>
 
-            <h2>Results:</h2>
-            ${
-                isClicked ?
-                result.length > 0 ?
-                html`<div class="search-result">
-                ${result.map(i => searchedCard(i, hasUser))}`
-                    
-                 : html`
-                 <p class="no-result">No result.</p>
-            </div>` : nothing}
-        </div>    
+          <h3>Results:</h3>
+
+          <div id="search-container">
+            <ul class="card-wrapper">
+              <!-- Display a li with information about every post (if any)-->
+              ${isClicked && result.length > 0 ?
+              result.map(i => searchedCard(i, user)): html`
+              <h2>There are no results found.</h2>`}
+            </ul>
+          </div>
         </section>`
 
 const searchedCard = (result, hasUser) => html`
-<div class="card-box">
-                    <img src=${result.imgUrl}>
-                    <div>
-                        <div class="text-center">
-                            <p class="name">${result.name}</p>
-                            <p class="artist">${result.artist}</p>
-                            <p class="genre">${result.genre}</p>
-                            <p class="price">${result.price}</p>
-                            <p class="date">${result.releaseDate}</p>
-                        </div>
-                        ${hasUser ?
-                            html`
-                            <div class="btn-group">
-                                <a href="/details/${result._id}" id="details">Details</a>
-                            </div>` : nothing
-                        }
-                        
-                    </div>
-                </div>`
+            <li class="card">
+                <img src=${result.imageUrl} alt="travis" />
+                <p>
+                  <strong>Brand: </strong><span class="brand">${result.brand}</span>
+                </p>
+                <p>
+                  <strong>Model: </strong
+                  ><span class="model">${result.model}</span>
+                </p>
+                <p><strong>Value:</strong><span class="value">${result.value}</span></p>
+                ${hasUser ? html`
+                <a class="details-btn" href="">Details</a>` : nothing}
+              </li>`
 
 
 export function showSearch(ctx) {
-    
+    const user = getUserData()
     ctx.render(searchTemplate(false, onSearch))
 
     async function onSearch(e) {
-        const searchedInput = document.getElementById('search-input')
+        e.preventDefault()
+        const searchedInput = document.getElementById('#search-input')
         const query = searchedInput.value
 
         if (!query) {
             return alert('Enter text!')
         }
        const result = await search(query)
-    
-       ctx.render(searchTemplate(true, onSearch, result, !!ctx.user))
+        
+       ctx.render(searchTemplate(true, onSearch, result ,user))
     }
 }
